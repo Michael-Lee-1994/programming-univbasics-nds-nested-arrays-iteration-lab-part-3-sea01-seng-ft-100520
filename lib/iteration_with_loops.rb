ae4a65928a823e188bf1e76e1ac88813c36d5948
Michael Lee <michael.l33.1994@gmail.com>
2020-09-22 17:23:37 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,7 +7,7 @@ def join_nested_strings(src)
   while i < src.length do
     j = 0 
     while j < src[i].length do
-      new_array.join(src[i][j])
+      new_array += src[i][j]
       j += 1
     end
     i += 1
